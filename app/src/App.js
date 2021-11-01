aec52dc2958ce023635f229a635436c1110109d5
Eduardo <jeseduardopi@gmail.com>
2021-11-01 09:45:36 +0100

    test completed

--- app/src/App.js
+++ app/src/App.js
@@ -1,41 +1,13 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import logo from './assets/logo.png'
+import React from 'react'
+import Home from './components/Home'
 
-class App extends Component {
-  render() {
+function App() {
     return (
-      <Main>
         <div>
-          <Header>
-            <Logo src={logo} alt="logo" />
-            <Title>Bienvenue sur le test front-end de Pretto</Title>
-          </Header>
-          <p>Vous trouverez toutes les instructions dans le README.md</p>
+        <Home />
+            
         </div>
-      </Main>
     )
-  }
 }
 
 export default App
-
-const Main = styled.main`
-  font-family: sans-serif;
-  text-align: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-`
-const Header = styled.header`
-  height: 150px;
-  padding: 24px;
-`
-const Logo = styled.img`
-  height: 80px;
-`
-const Title = styled.h1`
-  margin-top: 24px;
-  font-size: 32px;
-`
